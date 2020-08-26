8aace0c8e7e2ffaf4448c48c1771fbfb01171e88
aniboyadzhieva <anix0072@edu.easj.dk>
2020-08-26 12:42:15 +0200

    task 1

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Student
+    {
+        public string name;
+        public int birthMonth;
+        public int birthday;
+
+        public Student(string name, int birthMonth, int birthday)
+        {
+
+        }
+    }
+}
